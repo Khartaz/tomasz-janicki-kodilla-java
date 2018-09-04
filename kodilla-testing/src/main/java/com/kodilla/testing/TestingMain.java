1e500a87a48bcdde16f1fbaa79466c7959f1641d
Khartaz <tjaygallery@gmail.com>
2018-09-04 22:18:58 +0200

    ds

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
@@ -15,7 +16,25 @@ public class TestingMain {
         }
 
         System.out.println("Test - piewszy test jednostkowy: ");
+        // Nie wiem czy o to chodzilo...
+        Calculator calculator = new Calculator();
 
+        int result2 = calculator.add(2,4);
+        int result3 = calculator.subtract(4,3);
 
+        System.out.println(result2);
+        System.out.println(result3);
+
+        if(result2 == 6) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        if(result3 == 1) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
