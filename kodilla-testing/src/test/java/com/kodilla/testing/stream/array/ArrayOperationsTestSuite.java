a7993a392077a53bb88b0a76e4febb4c9e2a86c6
Khartaz <tjaygallery@gmail.com>
2018-09-21 18:07:06 +0200

    test Array

--- kodilla-testing/src/test/java/com/kodilla/testing/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,18 @@
 package com.kodilla.testing.stream.array;
 
+import com.kodilla.stream.array.ArrayOperations;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] numbers = {5,5,2};
+        //When
+        double actual = ArrayOperations.getAverage(numbers);
+        double expected = 4;
+        //Then
+        Assert.assertEquals(expected, actual, 0.00001);
+    }
 }
